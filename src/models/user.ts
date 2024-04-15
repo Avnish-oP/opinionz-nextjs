c87393bcaf1a3135a63c61328f4931554a937019
Avnish-oP <kavnish1245@gmail.com>
2024-04-15 11:06:43 +0530

    comment modelling and post modeling refined

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,42 +11,51 @@ export interface User extends Document {
   agreedPosts: string[];
   disagreedPosts: string[];
   comments: string[];
+  doddle: string;
 }
 
-export interface Post extends Document {
+export interface Comments extends Document {
+  comment_id: string;
   createdBy: string;
   content: string;
   createdAt: Date;
   agreedBy: string[];
   disagreedBy: string[];
-  comments: string[];
 }
 
-const userSchema:Schema<User> = new Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
+const userSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email id"],
+    unique: true,
+  },
   password: { type: String, required: true },
   userPosts: { type: [String], required: false },
   interests: { type: [String], required: false },
-  verified: { type: Boolean, required: true },
+  verified: { type: Boolean, required: false },
   createdAt: { type: Date, required: true },
   agreedPosts: { type: [String], required: false },
   disagreedPosts: { type: [String], required: false },
   comments: { type: [String], required: false },
+  doddle: { type: String, required: false },
 });
 
-const postSchema : Schema<Post> = new Schema({
+const commentSchema: Schema<Comments> = new Schema({
+  comment_id: { type: String, required: true },
   createdBy: { type: String, required: true },
   content: { type: String, required: true },
-  createdAt: { type: Date, required: true , default: Date.now()},
+  createdAt: { type: Date, required: true, default: Date.now() },
   agreedBy: { type: [String], required: false },
   disagreedBy: { type: [String], required: false },
-  comments: { type: [String], required: false },
 });
 
-
-export const UserModel = mongoose.model<User>("User", userSchema)|| mongoose.models.User;
-export const PostModel = mongoose.model<Post>("Post", postSchema) || mongoose.models.Post;
-
-
-
+export const UserModel =
+  mongoose.model<User>("User", userSchema) || mongoose.models.User;
+export const CommentModel =
+  mongoose.model<Comments>("Comment", commentSchema) || mongoose.models.Comment;
