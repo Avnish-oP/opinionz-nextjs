b1bd6330bd9e2de56aafa2c95f93625941f7f32a
bimlesharma <bimlesh.mdb@gmail.com>
2024-04-18 10:21:31 +0530

    feature section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,14 @@ import Features from '../components/Features';
 const Home = () => {
   return (
     <div>
-      <Head>
-        <title>opinionZ.com | Share Your Opinions Anonymously</title>
-        <link rel="icon" href="/favicon.ico" />
-        {/* Add meta tags for SEO */}
-      </Head>
       <Header />
       <Landing />
-      <main className="container mx-auto px-4">
+      
+      <main className="container mx-auto ">
         <Features />
       </main>
-      <footer className="bg-gray-950 py-8 text-center">
+      <footer className="bg-[#030712] pb-10 text-center">
+        <div className='h-[1px] mb-10 bg-purple-800'></div>
         <nav>
           <a href="#" className="px-4">FAQ</a>
           <a href="#" className="px-4">About Us</a>
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
-// import img1 from "@/images/anonymous-posting.svg";
+
 import img1 from "../../public/assets/img/anonymous-posting.svg";
 import img2 from "../../public/assets/img/community-engagement.svg";
 import img3 from "../../public/assets/img/moderation-tools.svg";
 import img4 from "../../public/assets/img/fact-verification.svg";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
+
+import MagicCard from "./MagicCard";
 
 const Features = () => {
   const images = [img1, img2, img3, img4];
@@ -17,17 +18,17 @@ const Features = () => {
       image: images[0],
     },
     {
-      title: "Community engagement tools",
+      title: "Engagement tools",
       description: "Engage with the community and participate in discussions",
       image: images[1],
     },
     {
-      title: "Moderation tools for a respectful environment",
+      title: "Respectful environment",
       description: "Ensure a respectful environment with moderation tools",
       image: images[2],
     },
     {
-      title: "AI-based fact's verification",
+      title: "AI-based facts' verification",
       description: "Verify facts using AI algorithms",
       image: images[3],
     },
@@ -35,27 +36,17 @@ const Features = () => {
   ];
 
   return (
-    <div className="my-10 min-h-[80vh] flex flex-col justify-center">
-      <h2 className="text-4xl font-bold text-center">Key Features</h2>
-      <section className="py-8 w-full flex justify-center">
-        <div className=" grid md:w-full w-[80%] lg:mx-20 md:mx-0 gap-5 lg:grid-cols-4  sm:grid-cols-1 md:grid-cols-2 text-lg">
+    <div className=" md:py-10 py-5  lg:min-h-[100vh] bg-[#030712] flex flex-col justify-center">
+      <h2 className="md:text-4xl text-3xl font-bold text-center my-5 md:my-10">Why to choose opinionZ.com?</h2>
+      <section className=" w-full flex justify-center">
+        <div className=" grid lg:w-[65%] md:w-[80%] w-[80%] lg:mx-20 md:mx-0 gap-3 lg:grid-cols-2  xl:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 text-lg">
           {features.map((feature, index) => (
-            <div
-              className=" border-4 rounded-xl hover:bg-[#171030ea] w-full p-5 hover:-translate-y-[2px] ease-in-out duration-200 hover:scale-[1.01]"
+            <MagicCard
               key={index}
-            >
-              <div className="h-[60%]">
-                <h3 className="font-bold text-2xl">{feature.title}</h3>
-                <p>{feature.description}</p>
-              </div>
-              <div className="flex flex-row justify-center w-full h-[40%]">
-                <Image
-                  className="w-32"
-                  src={feature.image}
-                  alt={feature.title}
-                />
-              </div>
-            </div>
+              title={feature.title}
+              icon={feature.image}
+              description={feature.description}
+            />
           ))}
         </div>
       </section>
